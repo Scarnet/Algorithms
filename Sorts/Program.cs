e9a6c5ec77ee29ca44378e24b7ddb076c0aa1bbe
mahmoud ahmed <mahmoud.elgawad@outlook.com>
2019-07-15 13:55:29 +0200

    Add project files.

---
+++ Sorts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sorts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 9, 6, 5, 2, 5, 3 };
+            arr = Sorter<int>.QuickSort(arr);
+            Console.Write("{");
+            foreach(var item in arr)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sorts/Sorter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorts
+{
+    public static class Sorter<T> where T : IComparable
+    {
+        private static T[] Shuffel(T[] arr)
+        {
+            var rand = new Random();
+            int n = arr.Length -1;
+            for(int i=0; i < arr.Length; i++)
+            {
+                int randIndex = rand.Next(0, n);
+                Swap(arr, i, randIndex);
+            }
+            return arr;
+        }
+
+
+        public static T[] QuickSort(T[] arr)
+        {
+            arr = Shuffel(arr);
+            Sort(arr, 0, arr.Length - 1);
+            return arr;
+        }
+
+        private static void Sort(T[] arr, int low, int high)
+        {
+            if (high <= low)
+                return;
+
+            int j = Partition(arr, low, high);
+            Sort(arr, low, j - 1);
+            Sort(arr, j + 1, high);
+        }
+
+        private static int Partition(T[] arr, int low, int high)
+        {
+            var key = arr[low];
+            int i = low + 1;
+            int j = high;
+        
+            while(true)
+            {
+                while(arr[i].CompareTo(key) < 0 && i < high)
+                    i++;
+
+                while (arr[j].CompareTo(key) > 0 && j > low)
+                    j--;
+
+
+                if (i >= j)
+                    break;
+
+                Swap(arr, i, j);
+            }
+
+            Swap(arr, low, j);
+            return j;
+        }
+
+        private static T[] Swap(T[] arr, int firstIndex, int secondIndex)
+        {
+            var temp = arr[firstIndex];
+            arr[firstIndex] = arr[secondIndex];
+            arr[secondIndex] = temp;
+            return arr;
+        }
+    }
+}
